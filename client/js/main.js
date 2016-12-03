bc0338b88052aaa5e2fad1b3d7ec07adf4146cc1
longlk-PC\longlk <llk@vacasol.com>
2016-12-03 16:51:00 +0700

    Detail page

---
+++ client/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#hinh-anh').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+})
+
+
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop();
+  $()
+}
