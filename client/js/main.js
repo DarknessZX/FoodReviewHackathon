72468bcde6a9b53f8352624cc620ef8e9d611f58
boy977 <troublemakr97@gmail.com>
2016-12-06 20:11:11 +0700

    no message

--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,3 @@
-$(document).ready(function(){
-  $('#hinh-anh').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
-})
-
-
-function onScroll(event) {
-  var scrollPos = $(document).scrollTop();
-  $()
-}
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
