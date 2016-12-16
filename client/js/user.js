78e63f3a7a4dcf0f745c34379b1bee5fed1c87bd
longlk-PC\longlk <llk@vacasol.com>
2016-12-17 02:27:24 +0700

    Add sign up for client

--- client/js/user.js
+++ client/js/user.js
@@ -2,16 +2,36 @@ function login(){
 	var formData = {
 		username: $('#username').val(),
 		password: $('#password').val()
-	}; 
-	console.log(formData);
+	};
+	//console.log(formData);
 	$.post("https://arcane-hollows-49001.herokuapp.com/api/user/login",
     formData,
     function(res){
         if(res.code) {
-
+					console.log(res);
         }
         else {
         	alert(res.message);
         }
     });
+}
+
+function signup(){
+	var formData = {
+		username: $('#username_signup').val(),
+		password: $('#password_signup').val(),
+		name : $('#name').val(),
+		age : $('#age').val()
+	};
+	console.log(formData);
+	$.post("/api/user/create",
+		formData,
+		function(res) {
+				if (res.status) {
+					console.log(res);
+				} else {
+					alert(res.message);
+				}
+		}
+	);
 }
